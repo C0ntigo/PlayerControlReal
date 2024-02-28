9316e6856f22902625c5496ffc642a3ecf4deb24
Xavier Real <720838@student.dusd.net>
2024-02-28 15:09:23 -0800

    Added the stuff
    start engines, pedal to the metal, High speed chase, Take the driver's seat I did all of these

--- P8PlayerControlReal/Assets/Scripts/PlayerController.cs
+++ P8PlayerControlReal/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float forwardInput;
+    private float horizontalInput;
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
         // Move the vechicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Rotates the car based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
