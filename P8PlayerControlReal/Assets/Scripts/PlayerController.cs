f085886f20334e1663d3db16b984a1d71706f440
Xavier Real <720838@student.dusd.net>
2024-03-06 12:20:35 -0800

    FINISHED THE GAME

--- P8PlayerControlReal/Assets/Scripts/PlayerController.cs
+++ P8PlayerControlReal/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
+
     private float forwardInput;
     private float horizontalInput;
     private float speed = 20.0f;
@@ -18,11 +22,18 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         forwardInput = Input.GetAxis("Vertical");
         horizontalInput = Input.GetAxis("Horizontal");
         // Move the vechicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         // Rotates the car based on horizontal input
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+        if(Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
     }
 }
